f797b8f7ffbc2d2aa8614bb89f7246fa3d4c7b71
RobK-WB <rob.kohout@warnerbros.com>
2023-03-28 15:23:57 -0700

    updated test scene, debug toggle

--- Assets/Oculus/SampleFramework/Usage/SceneManager/Scripts/VolumeAndPlaneSwitcher.cs
+++ Assets/Oculus/SampleFramework/Usage/SceneManager/Scripts/VolumeAndPlaneSwitcher.cs
@@ -4,23 +4,30 @@ using UnityEngine;
 [RequireComponent(typeof(OVRSceneAnchor))]
 public class VolumeAndPlaneSwitcher : MonoBehaviour
 {
-  public OVRSceneAnchor planePrefab;
-  public OVRSceneAnchor volumePrefab;
-  public enum GeometryType
-  {
+    public OVRSceneAnchor planePrefab;
+    public OVRSceneAnchor volumePrefab;
+
+    //TEST
+    public MeshRenderer meshRenderer;
+    public Material passthroughMaterial;
+    public Material debugMaterial;
+
+    public bool useDebugMaterial = false;
+    public enum GeometryType
+    {
     Plane,
     Volume,
-  }
-  [System.Serializable]
-  public struct LabelGeometryPair
-  {
+    }
+    [System.Serializable]
+    public struct LabelGeometryPair
+    {
     public string label;
     public GeometryType desiredGeometryType;
-  }
-  public List<LabelGeometryPair> desiredSwitches;
+    }
+    public List<LabelGeometryPair> desiredSwitches;
 
-  private void ReplaceAnchor(OVRSceneAnchor prefab, Vector3 position, Quaternion rotation, Vector3 localScale)
-  {
+    private void ReplaceAnchor(OVRSceneAnchor prefab, Vector3 position, Quaternion rotation, Vector3 localScale)
+    {
     var anchor = Instantiate(prefab, transform.parent);
     anchor.enabled = false; // disable so it won't update transform
     anchor.InitializeFrom(GetComponent<OVRSceneAnchor>());
@@ -28,16 +35,19 @@ public class VolumeAndPlaneSwitcher : MonoBehaviour
     anchor.transform.SetPositionAndRotation(position, rotation);
     foreach (Transform child in anchor.transform)
     {
-      child.localScale = localScale;
+        child.localScale = localScale;
     }
 
     Destroy(gameObject);
-  }
+    }
 
-  void Start()
-  {
-    var classification = GetComponent<OVRSemanticClassification>();
-    if (!classification) return;
+    void Start()
+    {
+#if UNITY_EDITOR
+        SetDebugMaterial(true);
+#endif
+        var classification = GetComponent<OVRSemanticClassification>();
+        if (!classification) return;
 
         Debug.LogError("LABEL:  " + classification.Labels[0]);
         //foreach (string label in classification.Labels)
@@ -47,87 +57,112 @@ public class VolumeAndPlaneSwitcher : MonoBehaviour
         //gameObject.tag = "Player";
         //   classification.
         foreach (LabelGeometryPair pair in desiredSwitches)
-    {
-      if (classification.Contains(pair.label))
-      {
-        Vector3 position = Vector3.zero;
-        Quaternion rotation = Quaternion.identity;
-        Vector3 localScale = Vector3.zero;
-        switch (pair.desiredGeometryType)
         {
-          case GeometryType.Plane:
-          {
-            var volume = GetComponent<OVRSceneVolume>();
-            if (!volume)
+            if (classification.Contains(pair.label))
             {
-              Debug.LogWarning($"Ignoring desired volume to plane switch for {pair.label} because it is not a volume.");
-              continue;
-            }
+                Vector3 position = Vector3.zero;
+                Quaternion rotation = Quaternion.identity;
+                Vector3 localScale = Vector3.zero;
+                switch (pair.desiredGeometryType)
+                {
+                    case GeometryType.Plane:
+                    {
+                        var volume = GetComponent<OVRSceneVolume>();
+                        if (!volume)
+                        {
+                            Debug.LogWarning($"Ignoring desired volume to plane switch for {pair.label} because it is not a volume.");
+                            continue;
+                        }
 
-            Debug.Log($"IN Volume Position {transform.position}, Dimensions: {volume.Dimensions}");
-            // This object is a volume, but we want a plane instead.
-            GetTopPlaneFromVolume(
-              transform,
-              volume.Dimensions,
-              out position,
-              out rotation,
-              out localScale);
-            Debug.Log($"OUT Plane Position {position}, Dimensions: {localScale}");
-            ReplaceAnchor(planePrefab, position, rotation, localScale);
-            break;
-          }
-          case GeometryType.Volume:
-          {
-            var plane = GetComponent<OVRScenePlane>();
-            if (!plane)
-            {
-              Debug.LogWarning($"Ignoring desired plane to volume switch for {pair.label} because it is not a plane.");
-              continue;
-            }
+                        Debug.Log($"IN Volume Position {transform.position}, Dimensions: {volume.Dimensions}");
+                        // This object is a volume, but we want a plane instead.
+                        GetTopPlaneFromVolume(
+                            transform,
+                            volume.Dimensions,
+                            out position,
+                            out rotation,
+                            out localScale);
+                        Debug.Log($"OUT Plane Position {position}, Dimensions: {localScale}");
+                        ReplaceAnchor(planePrefab, position, rotation, localScale);
+                        break;
+                    }
+                    case GeometryType.Volume:
+                    {
+                        var plane = GetComponent<OVRScenePlane>();
+                        if (!plane)
+                        {
+                            Debug.LogWarning($"Ignoring desired plane to volume switch for {pair.label} because it is not a plane.");
+                            continue;
+                        }
 
-            Debug.Log($"IN Plane Position {transform.position}, Dimensions: {plane.Dimensions}");
-            // This object is a plane, but we want a volume instead.
-            GetVolumeFromTopPlane(
-              transform,
-              plane.Dimensions,
-              transform.position.y,
-              out position,
-              out rotation,
-              out localScale);
-            Debug.Log($"OUT Volume Position {position}, Dimensions: {localScale}");
-            ReplaceAnchor(volumePrefab, position, rotation, localScale);
-            break;
-          }
+                        Debug.Log($"IN Plane Position {transform.position}, Dimensions: {plane.Dimensions}");
+                        // This object is a plane, but we want a volume instead.
+                        GetVolumeFromTopPlane(
+                            transform,
+                            plane.Dimensions,
+                            transform.position.y,
+                            out position,
+                            out rotation,
+                            out localScale);
+                        Debug.Log($"OUT Volume Position {position}, Dimensions: {localScale}");
+                        ReplaceAnchor(volumePrefab, position, rotation, localScale);
+                        break;
+                    }
+                }
+            }
         }
-      }
+        // IF we arrived here, no conversion was needed. Let's remove this component
+        //Destroy(this);
     }
-    // IF we arrived here, no conversion was needed. Let's remove this component
-    Destroy(this);
-  }
 
-  private void GetVolumeFromTopPlane(
+    private void GetVolumeFromTopPlane(
     Transform plane,
     Vector2 dimensions,
     float height,
     out Vector3 position,
     out Quaternion rotation,
     out Vector3 localScale)
-  {
-    position = plane.position;
-    rotation = plane.rotation;
-    localScale = new Vector3(dimensions.x, dimensions.y, height);
-  }
+    {
+        position = plane.position;
+        rotation = plane.rotation;
+        localScale = new Vector3(dimensions.x, dimensions.y, height);
+    }
 
-  private void GetTopPlaneFromVolume(
+    private void GetTopPlaneFromVolume(
     Transform volume,
     Vector3 dimensions,
     out Vector3 position,
     out Quaternion rotation,
     out Vector3 localScale)
-  {
-    float halfHeight = dimensions.y / 2.0f;
-    position = volume.position + Vector3.up * halfHeight;
-    rotation = Quaternion.LookRotation(Vector3.up, -volume.forward);
-    localScale = new Vector3(dimensions.x, dimensions.z, dimensions.y);
-  }
+    {
+        float halfHeight = dimensions.y / 2.0f;
+        position = volume.position + Vector3.up * halfHeight;
+        rotation = Quaternion.LookRotation(Vector3.up, -volume.forward);
+        localScale = new Vector3(dimensions.x, dimensions.z, dimensions.y);
+    }
+
+    private void Update()
+    {
+        //TEST
+        bool pressingButton = OVRInput.Get(OVRInput.RawButton.B);
+        if (pressingButton)
+        {
+            SetDebugMaterial(!useDebugMaterial);
+        }
+    }
+
+    public void SetDebugMaterial(bool debug)
+    {
+        useDebugMaterial = debug;
+        if (useDebugMaterial)
+        {
+            Debug.LogError("SETTING DEBUG MAT");
+            meshRenderer.material = debugMaterial;
+        }
+        else
+        {
+            Debug.LogError("SETTING PASSTHROUGH MAT");
+            meshRenderer.material = passthroughMaterial;
+        }        
+    }
 }
